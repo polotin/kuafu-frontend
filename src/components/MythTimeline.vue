fe1673e9661da1f50730846e4db6987691fb05b2
Renxingyin <renxingyin@xiamenair.com>
2019-08-08 11:01:29 +0800

    1、引入字体 2、时间线json

--- src/components/MythTimeline.vue
+++ src/components/MythTimeline.vue
@@ -1,24 +1,45 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <v-timeline>
+    <v-timeline dense>
         <v-timeline-item
-                v-for="n in 4"
-                :key="n"
+                v-for="(node, nodeIndex) in nodes"
+                :key="nodeIndex"
                 large
+                color="orange"
         >
             <template v-slot:icon>
-                <span>上古</span>
+                <span style="font-weight: bold;"
+                      :style="{
+                      'font-size':(node.node.length<3?node.node.length<2?'25':'17':'10') + 'px',
+                      'font-family': node.font}">
+                    {{node.node}}
+                </span>
             </template>
-            <v-card class="elevation-2">
-                <v-card-title class="headline">Lorem ipsum</v-card-title>
-                <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
+            <v-card width="95%">
+                <!--:color="nodeColors[nodeIndex/nodeColors.length]"-->
+                <v-card-title class="headline">{{node.title}}</v-card-title>
+                <v-card-text>
+                    {{node.intro}}
+                </v-card-text>
             </v-card>
         </v-timeline-item>
     </v-timeline>
 </template>
-
 <script>
+    import {nodes, nodeColors} from '../js/timeline'
+
     export default {
-        name: "MythTimeline"
+        name: "MythTimeline",
+        data() {
+            return {
+                nodes: [],
+                nodeColors: []
+            }
+        },
+
+        mounted() {
+            this.nodes = JSON.parse(nodes);
+            this.nodeColors = nodeColors;
+        }
     }
 </script>
 

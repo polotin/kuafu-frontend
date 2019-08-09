0f5b437c96292d552f55754fadc813ef11f18a6b
xavier_zy <mid_earth123@outlook.com>
2019-08-09 21:31:06 +0800

    建立空Home.vue，恢复一个icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,8 +10,9 @@ export default new Vuetify({
             books: 'mdi-library-books',
             wiki: 'mdi-chart-donut',
             myth: 'mdi-card-bulleted-settings-outline',
-            close:'mdi-window-close',
-            reference:'mdi-cube-outline',
+            close: 'mdi-window-close',
+            reference: 'mdi-cube-outline',
+            top: 'mdi-chevron-up'
         },
     },
 });
